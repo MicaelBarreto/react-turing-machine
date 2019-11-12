34efffe80957d355906cde6549d5dc00810d4ace
micaelbarreto <micaelmoraes.dev@gmail.com>
2019-11-12 11:11:16 -0300

    readme and setup

--- src/pages/setup/Setup.jsx
+++ src/pages/setup/Setup.jsx
@@ -5,8 +5,7 @@ function Setup() {
     const [estados, setEstados] = useState();
     const [entrada, setEntrada] = useState();
     const [fita, setFita] = useState();
-    const [transicoes, setTransicoes] = useState();
-    const [transicao, setTransicao] = useState();
+    const [transicoes, setTransicoes] = useState([]);
     const [countTransicao, setCountTransicao] = useState(1);
     const [inicial, setInicial] = useState();
     const [branco, setBranco] = useState();
@@ -33,8 +32,23 @@ function Setup() {
     }
 
     function handleAdd() {
-        
+        var transicoes = Object.values({...transicoes});
+        console.log(transicoes);
+        console.log(typeof transicoes);
+        transicoes.push({'estado': '', 'valor': '', 'transicao': {'estado': '', 'transicao': '', 'movimento': ''}});
+
+        setTransicoes(transicoes);
     }
+
+    function handleTransicao() {
+
+    }
+
+    function getTransicao(transicao) {
+        return `(${transicao.estado}, ${transicao.valor}) = (${transicao.transicao.estado}, ${transicao.transicao.valor}, ${transicao.transicao.movimento})`;
+    }
+    
+    
 
     // handleEntrada = event => {
     //     var split = event.split('');
@@ -49,28 +63,31 @@ function Setup() {
                     <InputSetup value={estados} onChange={e=> handleEstados(e.target.value)} placeholder="q0,q1,q2" />
                 </div>
                 <div className='form-control'>
-                    <label htmlFor="entrada">&Sigma;</label>
-                    <input type="text" className='form-input' id="entrada" value={entrada} onChange={e => setEntrada(e.target.value)} placeholder="110011" />
+                    <label htmlFor="entrada">&Sigma;</label>                    
+                    <InputSetup value={entrada} onChange={e => setEntrada(e.target.value)} placeholder="110011" />
                 </div>
                 <div className='form-control'>
-                    <label htmlFor="fita">&Gamma;</label>
-                    <input type="text" className='form-input' id="fita" value={fita} onChange={e => setFita(e.target.value)} placeholder="001100" />
+                    <label htmlFor="fita">&Gamma;</label>    
+                    <InputSetup value={fita} onChange={e => setFita(e.target.value)} placeholder="110011" />
                 </div>
                 <div className='form-control'>
                     <label htmlFor="inicial">q0</label>
-                    <input type="text" className='form-input' id="inicial" value={inicial} onChange={e => setInicial(e.target.value)} placeholder="q0" />
+                    <InputSetup value={inicial} onChange={e => setInicial(e.target.value)} placeholder="q0" />
                 </div>
                 <div className='form-control'>
                     <label htmlFor="branco">&empty;</label>
-                    <input type="text" className='form-input' id="branco" value={branco} onChange={e => setBranco(e.target.value)} placeholder="&empty;" />
+                    <InputSetup value={branco} onChange={e => setBranco(e.target.value)} placeholder="&empty;" />
                 </div>
                 <div className='form-control'>
                     <label htmlFor="finais">F</label>
-                    <input type="text" className='form-input' id="finais" value={finais} onChange={e => setFinal(e.target.value)} placeholder="q3,q4" />
+                    <InputSetup value={finais} onChange={e => setFinal(e.target.value)} placeholder="q3,q4" />
                 </div>
                 <div id="div-add">
                     <label htmlFor="transicao">&delta;</label>
-                    <button className="" id="btn-add" onClick={() => handleAdd()}>Adicionar</button>
+                    {transicoes.map(transicao => (
+                        <InputSetup value={() => getTransicao(transicao)} onChange={e => handleTransicao(e.target.value)} placeholder="q3,q4" />
+                    ))}
+                    <button type="button" className="" id="btn-add" onClick={() => handleAdd()}>Adicionar</button>
                 </div>
                 <div>
                     <button className="">Rodar</button>
